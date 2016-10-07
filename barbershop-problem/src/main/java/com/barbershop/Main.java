cf51128a1c7c1db2a709ae5930ea2e81df8cb534
Caio César <caio.cdcs@gmail.com>
2016-10-07 00:10:29 -0300

    Refactor to solve sleeping barber problem with payments

--- barbershop-problem/src/main/java/com/barbershop/Main.java
+++ barbershop-problem/src/main/java/com/barbershop/Main.java
@@ -1,21 +1,107 @@
 package com.barbershop;
 
 import java.util.Random;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
-/**
- * Created by caiocesar on 01/10/16.
- */
+
 public class Main {
-    public static final int NUM_OF_CHAIRS = 4;
-    public static final int NUM_OF_CUSTOMERS = 15;
 
-    public static void main(String args[]) throws InterruptedException {
-        PokemonCenter pokemonCenter = new PokemonCenter(NUM_OF_CHAIRS);
+    private static int customers = 0;
 
-        for (int i = 0; i < NUM_OF_CUSTOMERS; i++) {
-            (new Thread(new Pokemon(i, pokemonCenter))).start();
-            TimeUnit.SECONDS.sleep(Long.valueOf(new Random().nextInt(3)));
+    private static Semaphore mutex = new Semaphore(1);
+    private static Semaphore standingRoom = new Semaphore(12, true);
+    private static Semaphore sofa = new Semaphore(4, true);
+    private static Semaphore chair = new Semaphore(3);
+    private static Semaphore barber = new Semaphore(0);
+    private static Semaphore customer = new Semaphore(0);
+    private static Semaphore cash = new Semaphore(0);
+    private static Semaphore receipt = new Semaphore(0);
+
+    private static class Pokemon extends Thread {
+
+        private int id;
+
+        public Pokemon(int id) {
+            this.id = id;
+        }
+
+        public void run() {
+            try {
+                mutex.acquire();
+                if (customers == 16) {
+                    mutex.release();
+                    System.out.println(id + " foi embora");
+                    return;
+                }
+                customers++;
+                mutex.release();
+
+                standingRoom.acquire();
+                System.out.println(id + " entrou na sala de espera");
+
+                sofa.acquire();
+                System.out.println(id + " sentou no sofa");
+                standingRoom.release();
+
+                chair.acquire();
+                System.out.println(id + " sentou na cadeira do barbeiro");
+                sofa.release();
+
+                customer.release();
+                barber.acquire();
+                System.out.println(id + " cortando o cabelo");
+
+                System.out.println(id + " pagando o corte");
+                cash.release();
+                receipt.acquire();
+
+                chair.release();
+                mutex.acquire();
+                customers--;
+                mutex.release();
+                System.out.println(id + " saiu da barbearia");
+            } catch (Exception e) {
+
+            }
         }
     }
+
+    private static class Nurse extends Thread {
+
+        private int id;
+
+        public Nurse(int id) {
+            this.id = id;
+        }
+
+        public void run() {
+            while (true) {
+                try {
+                    customer.acquire();
+                    barber.release();
+                    System.out.println("Barbeiro " + id + " cortando cabelo");
+                    TimeUnit.SECONDS.sleep(new Random().nextInt(3));
+                    cash.acquire();
+                    System.out.println("Barbeiro  " + id + "  aceitando pagamento");
+                    receipt.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] adsa) throws InterruptedException {
+        for (int i = 0; i < 3; i++) {
+            (new Nurse(i)).start();
+        }
+
+        for (int i = 0; i < 40; i++) {
+            TimeUnit.SECONDS.sleep(new Random().nextInt(1));
+            (new Pokemon(i)).start();
+        }
+    }
+
 }
