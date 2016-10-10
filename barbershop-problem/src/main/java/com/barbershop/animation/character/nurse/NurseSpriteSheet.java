b76582a6fcddb53b9e7f65ffc8826820396acaca
Sabrina Beck Angelini <sabrinabeck.96@gmail.com>
2016-10-09 23:32:18 -0300

    remove guava

--- barbershop-problem/src/main/java/com/barbershop/animation/character/nurse/NurseSpriteSheet.java
+++ barbershop-problem/src/main/java/com/barbershop/animation/character/nurse/NurseSpriteSheet.java
@@ -3,9 +3,9 @@ package com.barbershop.animation.character.nurse;
 import com.barbershop.animation.character.CharacterSprite;
 import com.barbershop.animation.character.Orientation;
 import com.barbershop.animation.sprite.SpriteSheet;
-import com.google.common.collect.ImmutableList;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class NurseSpriteSheet extends SpriteSheet {
@@ -14,29 +14,29 @@ public class NurseSpriteSheet extends SpriteSheet {
     }
 
     public List<CharacterSprite> getNurseSprites(int x, int y, int width, int height) {
-        ImmutableList.Builder<CharacterSprite> builder = ImmutableList.builder();
+        List<CharacterSprite> result = new ArrayList<>();
 
-        builder.add(new CharacterSprite(getSprite(x, y, width, height), Orientation.DOWN));
-        builder.add(new CharacterSprite(getSprite(x + width, y, width, height), Orientation.DOWN));
-        builder.add(new CharacterSprite(getSprite(x + 2 * width, y, width, height), Orientation.DOWN));
-        builder.add(new CharacterSprite(getSprite(x + 3 * width, y, width, height), Orientation.DOWN));
+        result.add(new CharacterSprite(getSprite(x, y, width, height), Orientation.DOWN));
+        result.add(new CharacterSprite(getSprite(x + width, y, width, height), Orientation.DOWN));
+        result.add(new CharacterSprite(getSprite(x + 2 * width, y, width, height), Orientation.DOWN));
+        result.add(new CharacterSprite(getSprite(x + 3 * width, y, width, height), Orientation.DOWN));
 
-        builder.add(new CharacterSprite(getSprite(x, y + height, width, height), Orientation.LEFT));
-        builder.add(new CharacterSprite(getSprite(x + width, y + height, width, height), Orientation.LEFT));
-        builder.add(new CharacterSprite(getSprite(x + 2 * width, y + height, width, height), Orientation.LEFT));
-        builder.add(new CharacterSprite(getSprite(x + 3 * width, y + height, width, height), Orientation.LEFT));
+        result.add(new CharacterSprite(getSprite(x, y + height, width, height), Orientation.LEFT));
+        result.add(new CharacterSprite(getSprite(x + width, y + height, width, height), Orientation.LEFT));
+        result.add(new CharacterSprite(getSprite(x + 2 * width, y + height, width, height), Orientation.LEFT));
+        result.add(new CharacterSprite(getSprite(x + 3 * width, y + height, width, height), Orientation.LEFT));
 
-        builder.add(new CharacterSprite(getSprite(x, y + 2 * height, width, height), Orientation.RIGHT));
-        builder.add(new CharacterSprite(getSprite(x + width, y + 2 * height, width, height), Orientation.RIGHT));
-        builder.add(new CharacterSprite(getSprite(x + 2 * width, y + 2 * height, width, height), Orientation.RIGHT));
-        builder.add(new CharacterSprite(getSprite(x + 3 * width, y + 2 * height, width, height), Orientation.RIGHT));
+        result.add(new CharacterSprite(getSprite(x, y + 2 * height, width, height), Orientation.RIGHT));
+        result.add(new CharacterSprite(getSprite(x + width, y + 2 * height, width, height), Orientation.RIGHT));
+        result.add(new CharacterSprite(getSprite(x + 2 * width, y + 2 * height, width, height), Orientation.RIGHT));
+        result.add(new CharacterSprite(getSprite(x + 3 * width, y + 2 * height, width, height), Orientation.RIGHT));
 
-        builder.add(new CharacterSprite(getSprite(x, y + 3 * height, width, height), Orientation.UP));
-        builder.add(new CharacterSprite(getSprite(x + width, y + 3 * height, width, height), Orientation.UP));
-        builder.add(new CharacterSprite(getSprite(x + 2 * width, y + 3 * height, width, height), Orientation.UP));
-        builder.add(new CharacterSprite(getSprite(x + 3 * width, y + 3 * height, width, height), Orientation.UP));
+        result.add(new CharacterSprite(getSprite(x, y + 3 * height, width, height), Orientation.UP));
+        result.add(new CharacterSprite(getSprite(x + width, y + 3 * height, width, height), Orientation.UP));
+        result.add(new CharacterSprite(getSprite(x + 2 * width, y + 3 * height, width, height), Orientation.UP));
+        result.add(new CharacterSprite(getSprite(x + 3 * width, y + 3 * height, width, height), Orientation.UP));
 
-        return builder.build();
+        return result;
     }
 
 }
--- barbershop-problem/src/main/java/com/barbershop/animation/character/pokemon/PokemonSpriteSheet.java
+++ barbershop-problem/src/main/java/com/barbershop/animation/character/pokemon/PokemonSpriteSheet.java
@@ -3,9 +3,9 @@ package com.barbershop.animation.character.pokemon;
 import com.barbershop.animation.character.CharacterSprite;
 import com.barbershop.animation.character.Orientation;
 import com.barbershop.animation.sprite.SpriteSheet;
-import com.google.common.collect.ImmutableList;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PokemonSpriteSheet extends SpriteSheet {
@@ -14,7 +14,7 @@ public class PokemonSpriteSheet extends SpriteSheet {
     }
 
     public List<CharacterSprite> getCharacterSprites(int x, int y, int width, int height) {
-        ImmutableList.Builder<CharacterSprite> sprites = ImmutableList.builder();
+        List<CharacterSprite> sprites = new ArrayList<>();
 
         sprites.add(new CharacterSprite(getSprite(x, y, width, height), Orientation.UP));
         sprites.add(new CharacterSprite(getSprite(x, y + height, width, height), Orientation.UP));
@@ -28,7 +28,7 @@ public class PokemonSpriteSheet extends SpriteSheet {
         sprites.add(new CharacterSprite(getSprite(x + width, y + 2 * height, width, height), Orientation.RIGHT));
         sprites.add(new CharacterSprite(getSprite(x + width, y + 3 * height, width, height), Orientation.RIGHT));
 
-        return sprites.build();
+        return sprites;
     }
 
 }
